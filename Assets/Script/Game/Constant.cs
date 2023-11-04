482f4d9df4b05fb69bc5bac3ba6a965469bc9892
JoaquinLopezSoriano <jlopezsorian@uoc.edu>
2023-11-04 20:18:06 +0100

    V1

---
+++ Assets/Script/Game/Constant.cs
@@ -0,0 +1,31 @@
+namespace Script.Game
+{
+    /*
+     * Constant parameters
+     */
+    public static class Constant
+    {
+        // NavMesh name layers
+        public struct NavMesh
+        {
+            // Walkable name navMesh layer
+            public const string CITIZEN_WALK = "Walkable";
+        }
+
+        // Animation parameters
+        public struct Animation
+        {
+            public const string SPEED = "Speed";
+            public const string WAIT = "Wait";
+            public const string LEAVE = "Leave";
+        }
+        
+        // Collider Tags
+        public struct Collider
+        {
+            public const string BENCH = "Bench";
+        }
+   
+
+    }
+}
